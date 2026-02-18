aca1bf1b3fda9dc7e3639bf1e7985eba15b4043d
DonnoWanderlley <donnovanwanderlleycandelasramo@gmail.com>
2026-02-18 14:50:08 -0600

    Tarea etxtrema

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,54 @@
 module Helper exposing (..)
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+categoricalGrade : List number -> List GradeStatus
+categoricalGrade list =
+    List.map
+        (\grade ->
+            if grade >= 7 then
+                Approved
+
+            else if grade >= 0 && grade < 7 then
+                Failed
+
+            else
+                Pending
+        )
+        list
+
+
+-----------------------------------------------
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+        Delayed ->
+            "Esperar"
+
+        Cancelled ->
+            "Pedir reembolso"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction list =
+    List.map airplaneScheduleAction list
